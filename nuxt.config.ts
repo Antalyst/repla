ee7278f354b90ad1e1a73bd74253201657755467
unknown <Antalyst>
2026-03-04 10:28:59 +0800

    add url domain

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,8 @@ export default defineNuxtConfig({
       supabase: {
         url: process.env.SUPABASE_URL,
         key: process.env.SUPABASE_KEY,
-      }
+      },
+      apiBase: 'https://repla-theta.vercel.app'
     },
     geminiApiKey: process.env.GEMINI_API_KEY,
   }
